6fd9af215c3114164fe608b430016ac6150143de
kurz-m <m.kurz92@gmail.com>
2024-03-08 00:34:04 +0100

    minor fixes within the parser
    - change loop for parsing blocks to (current_token_.type & (RBRACE | EF)) == 0
    - add printing for the parsed config
    - add print to method from enum function
    - add reverse_map for config enums to string literals
    - enhance config file for more parsing checking

--- HTTP.cpp
+++ HTTP.cpp
@@ -1,19 +1,29 @@
 #include "HTTP.hpp"
 #include <algorithm>
+#include <iostream>
 
-method_e method_to_enum(std::string& method)
-{
-    const std::string method_arr[4] = {
-        "GET",
-        "POST",
-        "PUT",
-        "DELETE"
-    };
+method_e method_to_enum(std::string &method) {
+  const std::string method_arr[4] = {"GET", "POST", "PUT", "DELETE"};
 
-    for (size_t i = 0; i < 4; ++i) {
-        if (method == method_arr[i]) {
-            return static_cast<method_e>(1 << i);
-        }
+  for (size_t i = 0; i < 4; ++i) {
+    if (method == method_arr[i]) {
+      return static_cast<method_e>(1 << i);
     }
-    return UNKNOWN;
+  }
+  return UNKNOWN;
+}
+
+void print_method(int& method) {
+  if (method & GET) {
+    std::cout << "GET ";
+  }
+  if (method & POST) {
+    std::cout << "POST ";
+  }
+  if (method & PUT) {
+    std::cout << "PUT ";
+  }
+  if (method & DELETE) {
+    std::cout << "DELETE ";
+  }
 }

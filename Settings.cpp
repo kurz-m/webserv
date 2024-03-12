6064ec7310def5b2b22c60b48ae11c69603510fd
flauer <flauer@student.42heilbronn.de>
2024-03-12 09:59:33 +0000

    feat: correct behaviour for index and autoindex settings

--- Settings.cpp
+++ Settings.cpp
@@ -24,15 +24,15 @@ const Setting ServerBlock::find(Token::token_type_t name) const {
   throw NotFoundError();
 }
 
-const RouteBlock& ServerBlock::find(const std::string &path) const {
-  std::vector<RouteBlock>::const_iterator it;
-  for (it = routes.begin(); it != routes.end(); ++it) {
-    if (it->path == path) {
-      return *it;
-    }
-  }
-  throw NotFoundError();
-}
+// const RouteBlock& ServerBlock::find(const std::string &path) const {
+//   std::vector<RouteBlock>::const_iterator it;
+//   for (it = routes.begin(); it != routes.end(); ++it) {
+//     if (it->path == path) {
+//       return *it;
+//     }
+//   }
+//   throw NotFoundError();
+// }
 
 const Setting HttpBlock::find(Token::token_type_t name) const {
   std::vector<Setting>::const_iterator it;

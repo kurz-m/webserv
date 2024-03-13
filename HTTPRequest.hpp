b00cc9e0575f9189a2900dbe33b32bf297f759d5
flauer <flauer@student.42heilbronn.de>
2024-03-13 08:55:48 +0100

    wip cgi

--- HTTPRequest.hpp
+++ HTTPRequest.hpp
@@ -1,13 +1,13 @@
 #ifndef __HTTPREQUEST_HPP__
 #define __HTTPREQUEST_HPP__
 
+#include "HTTPBase.hpp"
 #include "Settings.hpp"
 #include "Socket.hpp"
-#include "HTTPBase.hpp"
 
 class HTTPRequest : public HTTPBase {
 public:
-  HTTPRequest(const ServerBlock&);
+  HTTPRequest(const ServerBlock &);
   HTTPRequest(const HTTPRequest &cpy);
   HTTPRequest &operator=(const HTTPRequest &other);
   ~HTTPRequest();
@@ -16,8 +16,12 @@ public:
   void parse_body();
 
 private:
+  std::map<std::string, std::string> parsed_header_;
+  method_e method_;
 
-friend class Socket;
+  friend class Socket;
+  friend class HTTPResponse;
+  friend class CGI;
 };
 
 #endif // __HTTPREQUEST_HPP__

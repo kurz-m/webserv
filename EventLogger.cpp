4c5ff23a01f29999264ffceb685ca5cfbce8167e
kurz-m <m.kurz92@gmail.com>
2024-03-19 11:54:18 +0000

    minor addition to verbose logger

--- EventLogger.cpp
+++ EventLogger.cpp
@@ -4,7 +4,12 @@
 
 EventLogger::EventLogger() {}
 
-EventLogger::EventLogger(const char *filename)
-{
-
+EventLogger::EventLogger(const char *filename) {
+  log_file_.open(filename, std::ios::app);
+  if (log_file_.is_open()) {
+    file_open_ = true;
+  }
 }
+
+void EventLogger::log(const std::string &message, const char *line,
+                      const char *file, log_e level) {}

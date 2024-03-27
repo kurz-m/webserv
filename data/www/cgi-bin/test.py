09c1303713e84be6931bb6a78e15bda3203908ee
flauer <flauer@student.42heilbronn.de>
2024-03-27 09:50:21 +0100

    implement post cgi and stdin to cgi

--- data/www/cgi-bin/test.py
+++ data/www/cgi-bin/test.py
@@ -6,8 +6,4 @@ import sys
 cgi.test()
 cgitb.enable()
 
-data = sys.stdin.buffer.read()
-with open('file.html', 'wb') as f:
-    f.write(data)
-print('Content-Type: text/plain\r\n\r\n', end='')
-print('Success!')
+print('success!')

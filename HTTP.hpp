c25ab1dd6e9fdd8af5b41d5750087663fd0acec9
flolau2k <florian.lauer@live.de>
2024-03-29 12:02:49 +0100

    fix mime types for status pages

--- HTTP.hpp
+++ HTTP.hpp
@@ -4,11 +4,12 @@
 #include <string>
 
 enum method_e {
-  GET = (1 << 0),     /**< Represents the http get method*/
-  POST = (1 << 1),    /**< Represents the http post method*/
-  PUT = (1 << 2),     /**< Represents the http put method*/
-  DELETE = (1 << 3),  /**< Represents the http delete method*/
-  UNKNOWN = (1 << 4), /**< Represents an unknow http method*/
+  GET = (1 << 0),       /**< Represents the http get method*/
+  POST = (1 << 1),      /**< Represents the http post method*/
+  PUT = (1 << 2),       /**< Represents the http put method*/
+  DELETE = (1 << 3),    /**< Represents the http delete method*/
+  UNKNOWN = (1 << 4),   /**< Represents an unknow http method*/
+  FORBIDDEN = (1 << 5), /**< Represents forbidden method in current location*/
 };
 
 method_e method_to_enum(std::string &method);

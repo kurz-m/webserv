7a1c7cb659c8390c2ec1c2003d2394e8f9efd4b1
flolau2k <florian.lauer@live.de>
2024-03-29 12:25:50 +0100

    remove unused

--- tests/webserv_test.py
+++ tests/webserv_test.py
@@ -43,37 +43,6 @@ def test_cgi(get_webserv_backend) -> None:
     assert res.status_code == 200
 
 
-# def test_stress(get_webserv_backend) -> None:
-#     global running
-#     running = True
-
-#     def test_worker() -> None:
-#         global running
-#         while running:
-#             res = requests.get("http://localhost:8080/")
-#             assert res.status_code == 200
-
-#     def worker_cgi() -> None:
-#         global running
-#         while running:
-#             res = requests.get("http://localhost:8080/cgi-bin/test.py")
-#             assert res.status_code == 200
-
-#     threads: List[Thread] = []
-#     for i in range(200):
-#         thread_ = Thread(target=test_worker)
-#         thread_.start()
-#         threads.append(thread_)
-#     for i in range(200):
-#         thread_ = Thread(target=worker_cgi)
-#         thread_.start()
-#         threads.append(thread_)
-#     time.sleep(30)
-#     running = False
-#     for thread in threads:
-#         thread.join()
-
-
 def test_autoindex(get_webserv_backend) -> None:
     res = requests.get("http://localhost:3490/")
     assert res.status_code == 200
